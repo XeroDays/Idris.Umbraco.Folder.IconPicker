99a94a2a58c9ff353949be7a24f1b233a59090fe
Sayed Muhammad Idrees <sayedidrees47@gmail.com>
2024-11-07 12:56:44 +0400

    Files added and tested

---
+++ Idris.Umbraco.Folder.IconPicker/App_Plugins/Idris.Umbraco.Folder.IconPicker/scripts/editor.controller.js
@@ -0,0 +1,43 @@
+﻿(function () {
+
+    angular.module('umbraco')
+        .controller('idris.foldericoneditorcontroller', ['editorService', 'assetsService', '$http', '$scope', icomoonEditorController]);
+
+    function icomoonEditorController(editorService, assetsService, $http, $scope) {
+
+        const $this = this;
+        $this.edit = edit;
+
+        function edit() {
+            var foldername = $scope.model.config.folderName;  
+
+            const options = {
+                title: 'Select icon from folder : wwwroot/' + foldername,
+                view: '/app_plugins/Idris.Umbraco.Folder.IconPicker/views/panel.html',
+                size: 'small',
+                folder : foldername,
+                submit: submit,
+                close: close,
+                value: $scope.model.value
+            }
+
+            editorService.open(options);
+        }
+
+        function submit(value) {
+
+            $scope.model.value = value;
+            close();
+        }
+
+        function close() {
+
+            editorService.close();
+        }
+
+        function init() {
+        }
+
+        init();
+    }
+})()
---
+++ Idris.Umbraco.Folder.IconPicker/App_Plugins/Idris.Umbraco.Folder.IconPicker/scripts/panel.controller.js
@@ -0,0 +1,36 @@
+﻿(function () {
+
+    angular.module('umbraco')
+        .controller('idris.foldericonpanelcontroller', ['$scope','$http','umbRequestHelper' ,icomoonPanelController]);
+
+    function icomoonPanelController($scope, $http,umbRequestHelper) {
+
+        const $this = this;
+        $this.close = close;
+        $this.select = select; 
+        //get folder here from editor controller
+
+        $this.icons = [];
+
+        function close() {
+
+            $scope.model.close();
+        }
+
+        function select(icon) {
+            $scope.model.submit(icon);
+        }
+
+        function init() { 
+            umbRequestHelper.resourcePromise( 
+                $http.get("/umbraco/backoffice/idris/UmbracoFolderIconPicker/GetAssetIcons?folder="+$scope.model.folder ),
+                    'Failed to retrieve images' ).then(function (response) { 
+                    $scope.icons = response.map(function (item) {
+                    $this.icons.push(item);
+                }); 
+             }); 
+        }
+
+        init();
+    }
+})()
